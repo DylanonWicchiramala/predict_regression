671d66c3e47a3924e1bd0739f90e8d298f904bd7
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2021-03-30 09:49:47 +0700

    None

--- Airq_main.py
+++ Airq_main.py
@@ -9,22 +9,26 @@ import pylab
 
 import pandas as pd
 from clean_dat import df,dropnull
+from mpl_toolkits import mplot3d
 
 #%%
 #plt.scatter([df['Time'][i].hour for i in range(len(df['Time'][0:100]))] , df['NOx(GT)'][0:100])
 #plt.show()
 #%%
 col = 'NOx(GT)'
-max = None
+max = (360)
 df_new = dropnull(df,col)
 #print(df_new.index)
 #%%
-X = np.array([i.hour for i in df_new["Time"][:max]])
-# X = np.array([df_new['Time'][i].hour for i in range(len(df_new['Time'][:max]))])
+X1 = np.array([i.hour for i in df_new["Time"][:max]])
+X2 = np.array([365*i.year + 31*i.month + i.day for i in df_new["Time"][:max]])
+print(X2)
 Y = np.array(df_new[col][:max])
-reg.regression_plot(X,Y, degree = 1, M=0)
-for i in range(1,25):
-    reg.regression_plot(X,Y, degree =i, M=0, dataplot = False)
+reg.regression_plot(X2,Y, degree = 1, margin=0)
 plt.show()
 
 # %%
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(X1,X2,Y)
+plt.show()
